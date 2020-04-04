57a8d3cffecd75171c4622273af0682f398195df
kelan <ddarzhain@gmail.com>
2020-04-05 00:15:48 +0700

    [admin] deleted Icons,Maps,RTL,UpgradeToPro

--- material-dashboard-react-master/src/routes.js
+++ material-dashboard-react-master/src/routes.js
@@ -24,17 +24,16 @@ import LocationOn from "@material-ui/icons/LocationOn";
 import Notifications from "@material-ui/icons/Notifications";
 import Unarchive from "@material-ui/icons/Unarchive";
 import Language from "@material-ui/icons/Language";
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import GroupIcon from '@material-ui/icons/Group';
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
 import TableList from "views/TableList/TableList.js";
 import Typography from "views/Typography/Typography.js";
-import Icons from "views/Icons/Icons.js";
-import Maps from "views/Maps/Maps.js";
 import NotificationsPage from "views/Notifications/Notifications.js";
 import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
-// core components/views for RTL layout
-import RTLPage from "views/RTLPage/RTLPage.js";
+
 
 const dashboardRoutes = [
   {
@@ -70,22 +69,6 @@ const dashboardRoutes = [
     layout: "/admin"
   },
   {
-    path: "/icons",
-    name: "Icons",
-    rtlName: "الرموز",
-    icon: BubbleChart,
-    component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    rtlName: "خرائط",
-    icon: LocationOn,
-    component: Maps,
-    layout: "/admin"
-  },
-  {
     path: "/notifications",
     name: "Notifications",
     rtlName: "إخطارات",
@@ -94,19 +77,19 @@ const dashboardRoutes = [
     layout: "/admin"
   },
   {
-    path: "/rtl-page",
-    name: "RTL Support",
-    rtlName: "پشتیبانی از راست به چپ",
-    icon: Language,
-    component: RTLPage,
-    layout: "/rtl"
+    path: "/educational-materials",
+    name: "Educational materials",
+    rtlName: "إخطارات",
+    icon: MenuBookIcon,
+    component: NotificationsPage,
+    layout: "/admin"
   },
   {
-    path: "/upgrade-to-pro",
-    name: "Upgrade To PRO",
-    rtlName: "التطور للاحترافية",
-    icon: Unarchive,
-    component: UpgradeToPro,
+    path: "/students",
+    name: "Students",
+    rtlName: "إخطارات",
+    icon: GroupIcon,
+    component: NotificationsPage,
     layout: "/admin"
   }
 ];
