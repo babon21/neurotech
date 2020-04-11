c52c8b75a42b22d3086def8bc34f3e509aed5bc6
kelan <ddarzhain@gmail.com>
2020-04-11 21:11:56 +0700

    [backend] Disciplines rest: post,put,delete,get.

--- backend/server.go
+++ backend/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -22,13 +25,117 @@ var uploadFormTmpl = []byte(`
 	</html>
 	`)
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(uploadFormTmpl)
-	})
+type Handler struct {
+	path string
+}
 
-	http.HandleFunc("/study", studyHandler)
-	http.HandleFunc("/study/delete", deleteHandler)
+type DisciplinePost struct {
+	Name string
+}
+
+type DisciplinePut struct {
+	OldName string `json:"old_name"`
+	NewName string `json:"new_name"`
+}
+
+type DisciplineDelete struct {
+	Name string
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		fmt.Println("Post request!")
+		h.CreateDiscipline(w, r)
+	case http.MethodDelete:
+		fmt.Println("Delete request!")
+		h.DeleteDiscipline(w, r)
+	case http.MethodPut:
+		fmt.Println("Put request!")
+		h.RenameDiscipline(w, r)
+	case http.MethodGet:
+		fmt.Println("Get request!")
+		h.GetDisciplineList(w, r)
+	}
+}
+
+func (h *Handler) CreateDiscipline(w http.ResponseWriter, r *http.Request) {
+	var post DisciplinePost
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result := CreateFolder(h.path + post.Name)
+	if result {
+		fmt.Println("Discipline created!")
+	} else {
+		fmt.Println("Failed create discipline!")
+	}
+}
+
+func (h *Handler) DeleteDiscipline(w http.ResponseWriter, r *http.Request) {
+	var delete DisciplineDelete
+	err := json.NewDecoder(r.Body).Decode(&delete)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err2 := os.Remove(h.path + delete.Name)
+	if err != nil {
+		fmt.Println(err2)
+		return
+	}
+	fmt.Println("Discipline deleted!")
+}
+
+func (h *Handler) RenameDiscipline(w http.ResponseWriter, r *http.Request) {
+	var put DisciplinePut
+	err := json.NewDecoder(r.Body).Decode(&put)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(put)
+
+	err2 := os.Rename(h.path+put.OldName, h.path+put.NewName)
+	if err != nil {
+		fmt.Println(err2)
+		return
+	}
+	fmt.Println("Discipline renamed!")
+}
+
+func (h *Handler) GetDisciplineList(w http.ResponseWriter, r *http.Request) {
+	files, err := ioutil.ReadDir(h.path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileNames := make([]string, 0, len(files))
+	for _, file := range files {
+		fmt.Println(file.Name())
+		fileNames = append(fileNames, file.Name())
+	}
+
+	fmt.Println(fileNames)
+	fileNamesJSON, err := json.Marshal(fileNames)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(fileNamesJSON)
+}
+
+func main() {
+	disciplineHandler := &Handler{path: "disciplines/"}
+	http.Handle("/disciplines", disciplineHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -67,11 +174,16 @@ func uploadFile(r *http.Request, path string, size int64) error {
 	return nil
 }
 
-func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	filename := r.FormValue("filename")
-	err := os.Remove("./study/" + filename)
-	if err != nil {
-		fmt.Println(err)
-		return
+func CreateFolder(folderName string) bool {
+	_, err := os.Stat(folderName)
+
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll(folderName, 0755)
+		if errDir != nil {
+			log.Fatal(err)
+			return false
+		}
+		return true
 	}
+	return false
 }
