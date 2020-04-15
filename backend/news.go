2e3fc297c1af1628a363bec1fdd761b97a8af05e
kelan <ddarzhain@gmail.com>
2020-04-15 12:30:05 +0700

    [backend] some mongo code

---
+++ backend/news.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/babon21/neurotech/backend/utils"
+	"github.com/rs/zerolog/log"
+)
+
+const NewsPath = "news/"
+
+type NewsHandler struct {
+	path string
+}
+
+type NewsPost struct {
+	Name string
+}
+
+type NewsPut struct {
+	OldName string `json:"old_name"`
+	NewName string `json:"new_name"`
+}
+
+type NewsDelete struct {
+	Name string
+}
+
+func (h *NewsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		fmt.Println("Post request!")
+		// h.CreateDiscipline(w, r)
+	case http.MethodDelete:
+		fmt.Println("Delete request!")
+		// h.DeleteDiscipline(w, r)
+	case http.MethodPut:
+		fmt.Println("Put request!")
+		// h.RenameDiscipline(w, r)
+	case http.MethodGet:
+		fmt.Println("Get request!")
+		// h.GetDisciplineList(w, r)
+	}
+}
+
+func (h *NewsHandler) CreateNews(w http.ResponseWriter, r *http.Request) {
+	var post DisciplinePost
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result := utils.CreateFolder(h.path + post.Name)
+	if result {
+		fmt.Println("Discipline created!")
+	} else {
+		fmt.Println("Failed create discipline!")
+	}
+}
+
+func (h *NewsHandler) DeleteNews(w http.ResponseWriter, r *http.Request) {
+	var delete DisciplineDelete
+	err := json.NewDecoder(r.Body).Decode(&delete)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err2 := os.Remove(h.path + delete.Name)
+	if err != nil {
+		fmt.Println(err2)
+		return
+	}
+	fmt.Println("Discipline deleted!")
+}
+
+func (h *NewsHandler) PutNews(w http.ResponseWriter, r *http.Request) {
+	var put DisciplinePut
+	err := json.NewDecoder(r.Body).Decode(&put)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(put)
+
+	err2 := os.Rename(h.path+put.OldName, h.path+put.NewName)
+	if err != nil {
+		fmt.Println(err2)
+		return
+	}
+	fmt.Println("Discipline renamed!")
+}
+
+func (h *NewsHandler) GetNewsList(w http.ResponseWriter, r *http.Request) {
+	files, err := ioutil.ReadDir(h.path)
+	if err != nil {
+		log.Err(err).Msg("read dir err")
+		return
+	}
+
+	fileNames := make([]string, 0, len(files))
+	for _, file := range files {
+		fmt.Println(file.Name())
+		fileNames = append(fileNames, file.Name())
+	}
+
+	fmt.Println(fileNames)
+	fileNamesJSON, err := json.Marshal(fileNames)
+
+	if err != nil {
+		log.Err(err).Msg("json marshall err")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(fileNamesJSON)
+}
--- backend/server.go
+++ backend/server.go
@@ -2,9 +2,40 @@ package main
 
 import (
 	"net/http"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
+type News struct {
+	Id      bson.ObjectId `json:"id" bson:" _id"`
+	Title   string        `json:"title" bson:"title"`
+	Content string        `json:"content" bson:"content"`
+}
+
 func main() {
+	session, err := mgo.Dial("mongodb://localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// если коллекции не будет, то она создасться автоматически
+	collection := session.DB("neurotech").C("news")
+
+	if n, _ := collection.Count(); n == 0 {
+		collection.Insert(&News{
+			bson.NewObjectId(),
+			"mongodb",
+			"Рассказать про монгу",
+		})
+		collection.Insert(&News{
+			bson.NewObjectId(),
+			"redis",
+			"Рассказать про redis",
+		})
+	}
+
 	disciplineHandler := &DisciplineHandler{path: DisciplinePath}
 	http.Handle("/disciplines", disciplineHandler)
 
