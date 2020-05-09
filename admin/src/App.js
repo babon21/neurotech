4594fc367af53ad74265f4db161d488ad7f9fa8b
kelan <ddarzhain@gmail.com>
2020-05-09 18:23:17 +0700

    [admin] publications

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
 import { NewsList, NewsEdit, NewsCreate } from './news';
+import { PublicationList, PublicationEdit, PublicationCreate } from './publications';
 import authProvider from './authProvider';
 
 
@@ -9,6 +10,7 @@ const dataProvider = simpleRestProvider('http://localhost:8080');
 const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
     <Resource name="news" list={NewsList} edit={NewsEdit} create={NewsCreate} />
+    <Resource name="publications" list={PublicationList} edit={PublicationEdit} create={PublicationCreate} />
   </Admin>
 );
 export default App;
---
+++ admin/src/publications.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { List, Datagrid, TextField, Create, Edit, TextInput, SimpleForm, NumberInput } from 'react-admin';
+
+
+export const PublicationList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="year" sortable={false}/>
+            <TextField source="title" sortable={false}/>
+        </Datagrid>
+    </List>
+);
+
+const PublicationTitle = ({ record }) => {
+    return <span>Publication {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PublicationEdit = props => (
+    <Edit title={<PublicationTitle />} {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <TextInput source="year" />
+            <TextInput multiline source="title" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PublicationCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <NumberInput source="year" />
+            <TextInput multiline source="title" />
+        </SimpleForm>
+    </Create>
+);
